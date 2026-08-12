a5953c3d9f8cf137c3bff7b783146d5a0f26c07f
JaidenGonzales <jmbgonzales31@src.pshs.edu.ph>
2026-08-12 14:21:45 +0800

    Add files via upload

---
+++ Q1_Molave_Gonzales (2).py
@@ -0,0 +1,24 @@
+import math
+# These are to input the X and Y value for the coordinates.
+x1 = float(input("Enter x1 Value: "))
+y1 = float(input("Enter y1 Value: "))
+x2 = float(input("Enter x2 Value: "))
+y2 = float(input("Enter y2 Value: "))
+
+
+#This is to calculate the distance between the 2 coordinates.
+dis = math.sqrt(pow(x2 - x1,2) + pow(y2 - y1,2))
+print(dis)
+
+
+
+#Reflection: 
+#When making this program i learned how to use square root in python coding. I also learned how to implement math into coding I also learned how to power numbers in code.
+
+
+
+
+
+
+
+
